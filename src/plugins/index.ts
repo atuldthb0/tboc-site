33223b1706a418069cbe5fceeffcab9e6034ffc8
atuldthb0 <thebigocommunity@disroot.org>
2026-02-11 21:08:08 +0530

    Checked and added back s3

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -44,10 +44,10 @@ export const plugins: Plugin[] = [
         })
       },
       hooks: {
-        afterChange: [revalidateRedirects],
+        afterChange: [revalidateRedirects as any],
       },
     },
-  }),
+  }) as any,
   nestedDocsPlugin({
     collections: ['categories'],
     generateURL: (docs) => docs.reduce((url, doc) => `${url}/${doc.slug}`, ''),
@@ -91,21 +91,21 @@ export const plugins: Plugin[] = [
       },
     },
   }),
-  //  s3Storage({
-  //   collections: {
-  //     media: {
-  //       prefix: 'media',
-  //     },
-  //   },  
-  //   bucket: process.env.S3_BUCKET || '',
-  //   config: {
-  //     forcePathStyle: true,
-  //     credentials: {
-  //       accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
-  //       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
-  //     },
-  //     region: process.env.S3_REGION || '',
-  //     endpoint: process.env.S3_ENDPOINT || '',
-  //   },
-  // }),
+   s3Storage({
+    collections: {
+      media: {
+        prefix: 'media',
+      },
+    },  
+    bucket: process.env.S3_BUCKET || '',
+    config: {
+      forcePathStyle: true,
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+      },
+      region: process.env.S3_REGION || '',
+      endpoint: process.env.S3_ENDPOINT || '',
+    },
+  }),
 ]
